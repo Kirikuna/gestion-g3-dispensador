9a24222eea508ea062e635f67dfa8c26729353a7
Leunam <manuelvaalram@gmail.com>
2022-04-27 19:24:12 -0400

    feat: edificios view

---
+++ frontend/components/BuildingCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card
+    class='mx-auto'
+    max-width='344'
+  >
+    <v-toolbar :color='building.color'>
+
+    </v-toolbar>
+    <v-card-title class='justify-center'>
+      {{ building.name }}
+    </v-card-title>
+
+    <v-card-text class='pa-0'>
+      <v-row>
+        <v-col cols='12' v-for='room in building.rooms' :key='room'>
+          <room-card class='room-card' :room='room' />
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['building'],
+  data: () => {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.room-card{
+  border: 20px black;
+}
+</style>
---
+++ frontend/components/RoomCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+    class='mx-auto'
+    max-width='344'
+  >
+    <v-row justify='space-between'>
+      <v-col cols='4'>
+        <v-card-title class='justify-center pa-0' style='font-size: 0.8em'>{{ room.name }}</v-card-title>
+      </v-col>
+      <v-col cols='4'>
+        <v-card-text class='pa-0'>{{"3-12-1999"}}</v-card-text>
+      </v-col>
+      <v-col cols='4'>
+        <v-card-actions class='pa-0 justify-center' >
+            <div
+              class="pa-3 rounded-circle d-inline-block"
+              :style='{backgroundColor: room.available ? "green": "red"}'
+            ></div>
+        </v-card-actions>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+<script>
+export default {
+  props: ['room'],
+};
+</script>
