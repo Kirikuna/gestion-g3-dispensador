92a7323e00464fb3acc38bb3053ae4f50a9d2739
Joaquin <49105542+OpusMangus@users.noreply.github.com>
2022-05-05 00:17:52 -0400

    (adv)Backed: get-edificio-salas now uses params instead of body

--- server/routes/salaRouter.js
+++ server/routes/salaRouter.js
@@ -6,7 +6,7 @@ const salaRouter = express.Router();
 salaRouter.post('/add-sala', salaController.addSala);
 salaRouter.get("/get-sala/:id", salaController.getSala);
 salaRouter.get("/get-all-salas", salaController.getAllSalas);
-salaRouter.get("/get-edificio-salas", salaController.getSalasOfEdificio);
+salaRouter.get("/get-edificio-salas/:eid", salaController.getSalasOfEdificio);
 salaRouter.put("/update-sala/:id", salaController.updateSala);
 salaRouter.delete("/delete-sala/:id", salaController.deleteSala);
 
