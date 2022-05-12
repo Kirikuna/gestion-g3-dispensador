2c4c08d8685fc4fb065cf879071d832f09d19202
Leunam <manuelvaalram@gmail.com>
2022-05-11 23:15:54 -0400

    hotfix: cors

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
-const express = require("express");
+const express = require('express');
 const routes = require('./routes/index');
-const cors = require('cors');
+//const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 
-app.use(cors())
+//app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
 
 // Configuring port
 const port = process.env.PORT || 9000;
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get("/", (req, res) => {
-  res.send("hello world trigger");
+app.get('/', (req, res) => {
+    res.send('hello world trigger');
 });
 routes(app);
 
