1a835789b6bd5087a69032ffa3a83a117dbfc915
Joaquin <49105542+OpusMangus@users.noreply.github.com>
2022-06-27 18:55:23 -0400

    (adv)frontend: Created component and page for successful report feedback.

---
+++ frontend/pages/successfulreport.vue
@@ -0,0 +1,15 @@
+<template>
+  <post-report></post-report>
+</template>
+
+<script>
+import PostReport from '~/components/reports/post-report';
+export default {
+  name: 'successfulreport',
+  components: { PostReport },
+};
+</script>
+
+<style scoped>
+
+</style>
